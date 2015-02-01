bd280ef9e9e2a0e88975172c4b3275f9f58ebfc1
ladislas <ladislas@weareleka.com>
2015-02-01 23:15:34 +0100

    the get command is now working. still needs some sexy error messages but it will do for now

--- unsplash.js
+++ unsplash.js
@@ -12,7 +12,21 @@ var inquirer = require('inquirer');
 var unsplashPath = path.resolve(homeFolder + '/.unsplash-cli');
 var databasePath = path.resolve(unsplashPath + '/database.json');
 var unsplashUrl = {
-	database: "https://unsplash.it/list"
+	root: "https://unsplash.it/",
+	database: "https://unsplash.it/list",
+}
+
+// Global functions
+function splitList(val) {
+	return val.split(',').map(Number);
+}
+
+function sanitizePath(pth) {
+	return path.resolve(pth);
+}
+
+function random(low, high) {
+	return Math.floor(Math.random() * (high - low + 1) + low);
 }
 
 // Check if database and directory exist
@@ -49,6 +63,15 @@ fs.open(databasePath, 'r', function (err, fd){
 	}
 });
 
+// Load database and get last id
+var databaseContent = fs.readFileSync(databasePath, 'utf8');
+var databaseJson = JSON.parse(databaseContent);
+var lastId = databaseJson[databaseJson.length-1].id;
+
+function randomImage() {
+	return random(0, lastId);
+}
+
 // Program General Config
 program
 .version('0.1.0')
@@ -76,13 +99,64 @@ program
 program
 .command('get')
 .description('download image')
-.option("-i, --id [value]", "image id")
-.option("-r, --random", "random image")
-.option("-p, --path [path]", "path to directory to store the image")
-.option("-s, --size <width> <height>", "image size <width> <height>")
-.action(function(id, options){
-	console.log('Download image with id %s to %s', id, options.path);
+.option("-i, --id [id]", "set image id")
+.option("-r, --random", "get random image id")
+.option("-f, --full", "full size image")
+.option("-s, --size <width>,<height>", "image size in pixels - default is 300,200", splitList, [300,200])
+.option("-p, --path [path]", "where the image should be stored", "./")
+.option("-n, --name [name]", "image name")
+.action(function(options){
+	var imageUrl;
+	var imageName;
+	var imageId;
+
+	if (options.id) {
+		imageId = options.id;
+	}
+	else if (options.random) {
+		imageId = randomImage();
+	}
+	else {
+		console.log("You must specify an image id or choose a random one..."); return;
+	}
+
+	if (options.full) {
+		imageUrl = databaseJson[imageId].post_url + "/download";
+	}
+	else if (options.size) {
+		imageUrl = unsplashUrl.root + options.size[0] + "/" + options.size[1] + "?image=" + imageId;
+	}
+	else {
+		imageUrl = databaseJson[imageId].post_url + "/download";
+	}
+
+	if (options.name) {
+		imageName = options.name + "." + databaseJson[imageId].format;
+	}
+	else {
+		imageName = databaseJson[imageId].filename;
+	}
+
+	var imageDirPath = sanitizePath(options.path);
+	var imagePath = path.join(imageDirPath, imageName);
+
+	request(imageUrl, {encoding: 'binary'}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			fs.mkdirs(imageDirPath, function (err) {
+				if(err) console.log("Unable to create %s to store the image", imageDirPath);
+			})
+			fs.writeFile(imagePath, body, 'binary', function (err) {
+				if (err) console.log("\nUnable to save the picture %s to %s, from %s, please try again...", imageId, imagePath, imageUrl);
+				else console.log("\nSuccessfully saved picture %s to %s from %s!", imageId, imagePath, imageUrl);
+			});
+		}
+		else {
+			console.log("\nUnable to save the picture %s to %s, from %s, please try again...", imageId, imagePath, imageUrl);
+			// console.log("\nUnable to save the picture %s to %s, please make sure you are connected to the internet and try again...", options.id, options.path);
+		}
+	});
 });
+
 
 // Program Get Link To Image
 program
