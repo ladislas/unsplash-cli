c691d9f2304b671445cbc9f4c1d1daea0e9b962a
ladislas <ladislas@weareleka.com>
2015-02-03 22:30:58 +0100

    put everything into the callback makes it run smoothly :)

--- unsplash.js
+++ unsplash.js
@@ -10,18 +10,16 @@ var inquirer = require('inquirer');
 var clipboard = require('copy-paste');
 var chalk = require('chalk');
 
+
 // Global variables
 var unsplashPath = path.resolve(homeFolder + '/.unsplash-cli');
 var databasePath = path.resolve(unsplashPath + '/database.json');
-
-var databaseContent;
-var databaseJson;
-var lastId;
 
 var unsplashUrl = {
 	root: "https://unsplash.it/",
 	database: "https://unsplash.it/list",
 }
+
 
 // Global functions
 function splitList(val) {
@@ -36,7 +34,8 @@ function random(low, high) {
 	return Math.floor(Math.random() * (high - low + 1) + low);
 }
 
-// Check if database and directory exist
+
+// Prepare inquirer questions
 var installQuestions = [{
 	type: "confirm",
 	name: "installDatabase",
@@ -45,6 +44,7 @@ var installQuestions = [{
 	default: false
 }];
 
+// Launch cli
 fs.readFile(databasePath, 'utf8', function (err, data){
 	if (err) {
 		console.log(installQuestions[0].premessage);
@@ -69,158 +69,146 @@ fs.readFile(databasePath, 'utf8', function (err, data){
 		});
 	}
 	else {
-		databaseContent = data;
-		databaseJson = JSON.parse(databaseContent);
-		lastId = databaseJson[databaseJson.length-1].id;
-	}
-});
 
-// Load database and get last id
-// var databaseContent = fs.readFileSync(databasePath, 'utf8');
-// var databaseJson = JSON.parse(databaseContent);
-// var lastId = databaseJson[databaseJson.length-1].id;
+		var databaseContent = data;
+		var databaseJson = JSON.parse(databaseContent);
+		var lastId = databaseJson[databaseJson.length-1].id;
 
-function randomImage() {
-	return random(0, lastId);
-}
+		function randomImage() {
+			return random(0, lastId);
+		}
 
-// Program General Config
-program
-.version('0.1.0')
-.usage('[command] [option] [arguments]');
+		// Program General Config
+		program
+			.version('0.1.0')
+			.usage('[command] [options] [arguments]');
 
-// Program Update Database
-program
-.command('update')
-.description('update image database')
-.action(function(){
-	request(unsplashUrl.database, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			fs.writeFile(databasePath, body, function (err) {
-				if (err) console.log(chalk.red("\nUnable to write the new database, make sure it is not already in use."));
-				else console.log(chalk.green("\nThe database has been successfully updated, you can now enjoy your new pictures!"))
+		// Program Update Database
+		program
+			.command('update')
+			.description('update image database')
+			.action(function(){
+				request(unsplashUrl.database, function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						fs.writeFile(databasePath, body, function (err) {
+							if (err) console.log(chalk.red("\nUnable to write the new database, make sure it is not already in use."));
+							else console.log(chalk.green("\nThe database has been successfully updated, you can now enjoy your new pictures!"))
+						});
+					}
+					else {
+						console.log(chalk.red("\nUnable to update the new database, make sure you are connected to the internet."));
+					}
+				});
 			});
-		}
-		else {
-			console.log(chalk.red("\nUnable to update the new database, make sure you are connected to the internet."));
-		}
-	});
-});
 
-// Program Download Images
-program
-.command('get')
-.description('download image')
-.option("-i, --id [id]", "set image id")
-.option("-r, --random", "get random image id")
-.option("-f, --full", "full size image")
-.option("-s, --size <width>,<height>", "image size in pixels - default is 300,200", splitList, [300,200])
-.option("-p, --path [path]", "where the image should be stored", "./")
-.option("-I, --image-name [name]", "image name")
-.option("-l, --link", "get picture link and copy to clipboard")
-.action(function(options){
-	var imageUrl;
-	var imageName;
-	var imageId;
+		// Program Download Images
+		program
+			.command('get')
+			.description('download image')
+			.option("-i, --id [id]", "set image id")
+			.option("-r, --random", "get random image id")
+			.option("-f, --full", "full size image")
+			.option("-s, --size <width>,<height>", "image size in pixels - default is 300,200", splitList, [300,200])
+			.option("-p, --path [path]", "where the image should be stored", "./")
+			.option("-I, --image-name [name]", "image name")
+			.option("-l, --link", "get picture link and copy to clipboard")
+			.action(function(options){
 
-	if (options.id) {
-		imageId = options.id;
-	}
-	else if (options.random) {
-		imageId = randomImage();
-	}
-	else {
-		console.log(chalk.red("\nYou must specify an image --id or choose a --random one"));
-		return;
-	}
+				var imageUrl;
+				var imageName;
+				var imageId;
 
-	if (options.full) {
-		imageUrl = databaseJson[imageId].post_url + "/download";
-	}
-	else if (options.size) {
-		imageUrl = unsplashUrl.root + options.size[0] + "/" + options.size[1] + "?image=" + imageId;
-	}
-	else {
-		imageUrl = databaseJson[imageId].post_url + "/download";
-	}
+				if (options.id) {
+					imageId = options.id;
+				}
+				else if (options.random) {
+					imageId = randomImage();
+				}
+				else {
+					console.log(chalk.red("\nYou must specify an image --id or choose a --random one"));
+					return;
+				}
 
-	if (options.imageName) {
-		imageName = options.imageName + "." + databaseJson[imageId].format;
-	}
-	else {
-		imageName = databaseJson[imageId].filename;
-	}
+				if (options.full) {
+					imageUrl = databaseJson[imageId].post_url + "/download";
+				}
+				else if (options.size) {
+					imageUrl = unsplashUrl.root + options.size[0] + "/" + options.size[1] + "?image=" + imageId;
+				}
+				else {
+					imageUrl = databaseJson[imageId].post_url + "/download";
+				}
 
-	var imageDirPath = sanitizePath(options.path);
-	var imagePath = path.join(imageDirPath, imageName);
+				if (options.imageName) {
+					imageName = options.imageName + "." + databaseJson[imageId].format;
+				}
+				else {
+					imageName = databaseJson[imageId].filename;
+				}
 
-	request(imageUrl, {encoding: 'binary'}, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			fs.mkdirs(imageDirPath, function (err) {
-				if(err) console.log("Unable to create %s to store the image", imageDirPath);
-			})
-			fs.writeFile(imagePath, body, 'binary', function (err) {
-				if (err) console.log("\nUnable to save the picture %s to %s, from %s, please try again...", imageId, imagePath, imageUrl);
-				else console.log("\nSuccessfully saved picture %s to %s from %s!", imageId, imagePath, imageUrl);
+				var imageDirPath = sanitizePath(options.path);
+				var imagePath = path.join(imageDirPath, imageName);
+
+				request(imageUrl, {encoding: 'binary'}, function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						fs.mkdirs(imageDirPath, function (err) {
+							if(err) console.log(chalk.red("\nUnable to create the directory to store the image, make sure you have the right permissions."), imageDirPath);
+						})
+						fs.writeFile(imagePath, body, 'binary', function (err) {
+							if (err) console.log(chalk.red("\nUnable to save the image %s to %s, from %s, please try again."), imageId, imagePath);
+							else console.log(chalk.green("\nThe image %s has been successfully saved to %s from %s!"), imageId, imagePath);
+						});
+					}
+					else {
+						console.log(chalk.red("\nUnable to save image %s to %s, from %s, please make sure you are connected to the internet and try again."), imageId, imagePath, imageUrl);
+					}
+				});
+
+				if (options.link) {
+					clipboard.copy(imageUrl, function (err) {
+						if (err) console.log(chalk.red("\nUnable to copy the link to clipboard..."));
+						else console.log(chalk.green("\nLink successfully copied to clipboard"));
+					})
+				}
 			});
-		}
-		else {
-			console.log("\nUnable to save the picture %s to %s, from %s, please try again...", imageId, imagePath, imageUrl);
-			// console.log("\nUnable to save the picture %s to %s, please make sure you are connected to the internet and try again...", options.id, options.path);
-		}
-	});
 
-	if (options.link) {
-		clipboard.copy(imageUrl, function (err) {
-			if (err) console.log("Unable to copy the link to clipboard...");
-			else console.log("Link successfully copied to clipboard");
-		})
+
+		// Program Get Link To Image
+
+		// Program Set Deskop With Image
+		program
+			.command('desktop')
+			.description('get image link')
+			.option("-i, --id [value]", "get image with id")
+			.option("-r, --random", "get a random image")
+			.option("-p, --path [path]", "path to directory to store the image")
+			.option("-s, --size <width> <height>", "choose image size")
+			.action(function(id, options){
+				console.log('Download image with id %s to %s', id, options.path);
+			});
+
+		program
+			.command('exec <cmd>')
+			.alias('ex')
+			.description('execute the given remote cmd')
+			.option("-e, --exec_mode <mode>", "Which exec mode to use")
+			.action(function(cmd, options){
+				console.log('exec "%s" using %s mode', cmd, options.exec_mode);
+			}).on('--help', function() {
+				console.log('  Examples:');
+				console.log();
+				console.log('    $ deploy exec sequential');
+				console.log('    $ deploy exec async');
+				console.log();
+			});
+
+		program
+			.command('*')
+			.action(function(env){
+				console.log('deploying "%s"', env);
+			});
+
+		program.parse(process.argv);
 	}
 });
 
-
-// Program Get Link To Image
-program
-.command('link')
-.description('get image link and copy to clipboard')
-.option("-i, --id [value]", "image id")
-.option("-r, --random", "random image")
-.option("-s, --size <width> <height>", "choose image size")
-.action(function(id, options){
-	console.log('Download image with id %s to %s', id, options.path);
-});
-
-// Program Set Deskop With Image
-program
-.command('desktop')
-.description('get image link')
-.option("-i, --id [value]", "get image with id")
-.option("-r, --random", "get a random image")
-.option("-p, --path [path]", "path to directory to store the image")
-.option("-s, --size <width> <height>", "choose image size")
-.action(function(id, options){
-	console.log('Download image with id %s to %s', id, options.path);
-});
-
-program
-.command('exec <cmd>')
-.alias('ex')
-.description('execute the given remote cmd')
-.option("-e, --exec_mode <mode>", "Which exec mode to use")
-.action(function(cmd, options){
-	console.log('exec "%s" using %s mode', cmd, options.exec_mode);
-}).on('--help', function() {
-	console.log('  Examples:');
-	console.log();
-	console.log('    $ deploy exec sequential');
-	console.log('    $ deploy exec async');
-	console.log();
-});
-
-program
-.command('*')
-.action(function(env){
-	console.log('deploying "%s"', env);
-});
-
-program.parse(process.argv);
